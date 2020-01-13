c3fe0ccbbfc301bac7b2fb47ceb599581ff9d4df
AlekseyK <aleksey.kabishau@gmail.com>
2020-01-12 19:25:26 -0500

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Aleksey on 0112..20.
+//  Copyright © 2020 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
