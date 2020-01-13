4545a4afd1dff072c93619e1f367239aba58a551
AlekseyK <aleksey.kabishau@gmail.com>
2020-01-13 00:08:35 -0500

    feat: Add navigation structure to project

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Aleksey on 0112..20.
+//  Copyright © 2020 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
--- GitHubFollowers/SearchVC.swift
+++ GitHubFollowers/SearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GitHubFollowers
 //
 //  Created by Aleksey on 0112..20.
@@ -8,14 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .systemPink
+
+        view.backgroundColor = .systemTeal
     }
-
-
 }
-
