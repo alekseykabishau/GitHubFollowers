64710bc35d392ca55933ce2987365354c63e5c2a
AlekseyK <aleksey.kabishau@gmail.com>
2020-01-13 23:55:54 -0500

    feat: Add custom reusable button

---
+++ GitHubFollowers/GFButton.swift
@@ -0,0 +1,36 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Aleksey on 0113..20.
+//  Copyright © 2020 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    // required to handle initialization via storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero) // it will be changed with constraints
+        self.backgroundColor = backgroundColor
+        self.titleLabel?.text = title
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
