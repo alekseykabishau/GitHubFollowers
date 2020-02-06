b0426eb0a3e3f64e15f3aa9de3170a8ed8b675f3
AlekseyK <aleksey.kabishau@gmail.com>
2020-02-05 23:07:19 -0500

    refactor: Move layout configuration into the helper

---
+++ GitHubFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,26 @@
+//
+//  UIHelper.swift
+//  GitHubFollowers
+//
+//  Created by Aleksey on 0205..20.
+//  Copyright © 2020 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+struct UIHelper {
+    
+    static func createThreeColumnFlowLayout(view: UIView) -> UICollectionViewFlowLayout {
+        
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minItemSpacing: CGFloat = 10
+        let availableWidth = width - padding * 2 - minItemSpacing * 2
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        return flowLayout
+    }
+}
