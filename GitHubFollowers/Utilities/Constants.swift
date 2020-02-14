e12441e3505dc4a6da0618d93f21e3290a35e4dc
AlekseyK <aleksey.kabishau@gmail.com>
2020-02-13 23:46:35 -0500

    feat: Add child header vc for user info screen + constants file

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Aleksey on 0213..20.
+//  Copyright © 2020 Aleksey Kabishau. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
