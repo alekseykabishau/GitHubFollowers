2b45522ba31f8508f9f343317f5642197cc22bd3
AlekseyK <aleksey.kabishau@gmail.com>
2020-03-14 20:29:07 -0400

    feat: add custom favorite table view cell

---
+++ GitHubFollowers/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,57 @@
+//
+//  FavoriteCell.swift
+//  GitHubFollowers
+//
+//  Created by Aleksey on 0314..20.
+//  Copyright © 2020 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    static let reuseID = "FavoriteCell"
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let userNameLable = GFTitleLabel(textAlignment: .left, fontSize: 26)
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func set(favorite: Follower) {
+        userNameLable.text = favorite.login
+        avatarImageView.downloadImage(from: favorite.avatarUrl)
+    }
+    
+    
+    private func configure() {
+        
+        addSubview(avatarImageView)
+        addSubview(userNameLable)
+        
+        accessoryType = .disclosureIndicator
+        
+        let padding: CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            
+            userNameLable.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+            userNameLable.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: padding * 2),
+            userNameLable.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            userNameLable.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+}
