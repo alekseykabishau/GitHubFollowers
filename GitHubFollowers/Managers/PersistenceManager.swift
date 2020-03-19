c65b7d165983df3a02aff0835cc14ad2cb3db5f5
AlekseyK <aleksey.kabishau@gmail.com>
2020-03-18 23:15:39 -0400

    refactor: remove unnecessary temp array

--- GitHubFollowers/Managers/PersistenceManager.swift
+++ GitHubFollowers/Managers/PersistenceManager.swift
@@ -26,23 +26,22 @@ struct PersistenceManager {
         
         retrieveFavorites { (result) in
             switch result {
-            case .success(let favorites):
-                var retrievedFavorites = favorites
+            case .success(var favorites):
                 
                 switch actionType {
                 case .add:
-                    guard !retrievedFavorites.contains(favorite) else {
+                    guard !favorites.contains(favorite) else {
                         completed(.alreadyInFavorites)
                         return
                     }
-                    retrievedFavorites.append(favorite)
+                    favorites.append(favorite)
                 case .remove:
-                    retrievedFavorites.removeAll { (follower) -> Bool in
+                    favorites.removeAll { (follower) -> Bool in
                         follower.login == favorite.login
                     }
                 }
                 
-                completed(save(favorites: retrievedFavorites))
+                completed(save(favorites: favorites))
                 
             case .failure(let error):
                 completed(error)
