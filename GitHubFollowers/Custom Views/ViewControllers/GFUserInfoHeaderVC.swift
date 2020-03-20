b8fc10262f594028927edc27a6ca9d617cb62b03
AlekseyK <aleksey.kabishau@gmail.com>
2020-03-19 20:58:38 -0400

    refactor: move repetitive download avatar networking call into function

--- GitHubFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
+++ GitHubFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -37,12 +37,7 @@ class GFUserInfoHeaderVC: UIViewController {
     
     
     func configureUIElements() {
-        
-        NetworkManager.shared.downloadImage(from: user.avatarUrl) { [weak self] image in
-            guard let self = self else { return }
-            DispatchQueue.main.async { self.avatarImageView.image = image }
-        }
-        
+        avatarImageView.downloadImage(fromURL: user.avatarUrl)
         usernameLabel.text = user.login
         nameLabel.text = user.name ?? ""
         locationImageView.image = SFSymbols.location
